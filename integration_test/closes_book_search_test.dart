1b9e15bf626b7fb46e8c77667eb260f2ec0c4cdd
Leonardo <leonardoorosaa16@gmail.com>
2023-03-31 09:32:48 -0300

    feat(books): added tests for new book item with a lot of authors

--- integration_test/closes_book_search_test.dart
+++ integration_test/closes_book_search_test.dart
@@ -1,5 +1,4 @@
 import 'package:books/presentation/presentation.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'common.dart';
--- test/presentation/widgets/new_book_item/new_book_item_golden_test.dart
+++ test/presentation/widgets/new_book_item/new_book_item_golden_test.dart
@@ -13,7 +13,7 @@ void main() {
 
     testGoldens(
       'displayed the new book item',
-      filename: 'new_item_book',
+      filename: 'new_book_item',
       constraints: const BoxConstraints(
         maxHeight: 120,
         maxWidth: 365,
@@ -28,6 +28,31 @@ void main() {
             title: 'The Lightning Thief',
             releasedAt: DateTime(2005, 06, 28),
             authors: const ['Rick Riordann'],
+          ),
+        ),
+      ),
+    );
+
+    testGoldens(
+      'displayed the new book item with a lot of authors',
+      filename: 'new_book_item_lot_authors',
+      constraints: const BoxConstraints(
+        maxHeight: 120,
+        maxWidth: 365,
+      ),
+      builder: () => ColoredBox(
+        color: Colors.white,
+        child: NewBookItem(
+          Book(
+            id: 1,
+            imageUrl:
+                'https://fastly.picsum.photos/id/306/200/200.jpg?hmac=_MA2OQbvCf09ghW0BrkSYh9mOhP-xpHqg2c5joDIRFg',
+            title: 'The Lightning Thief',
+            releasedAt: DateTime(2005, 06, 28),
+            authors: const [
+              'Rick Riordann',
+              'Rick Riordann',
+            ],
           ),
         ),
       ),
